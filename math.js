02e9761d5047b52db9f792323a43dca1e64ebc18
Germán Leiva <leiva@cavi.au.dk>
2021-02-16 13:35:16 +0100

    Added code to draw a grid

--- math.js
+++ math.js
@@ -5,16 +5,29 @@ canvas.width = width
 let height = window.innerHeight
 canvas.height = height
 
-for (let index = 0; index < 100; index++) {
+// for (let index = 0; index < 100; index++) {
+//     context.beginPath()
+//     context.moveTo(Math.random() * width, Math.random() * height)
+//     context.lineTo(Math.random() * width, Math.random() * height)
+//     context.stroke()
+// }
+
+//Make horizontal lines (10px distance between each other) by copying lines 8 to 13 and modifiying the code
+for (let w = 0; w < width; w+=10) {
     context.beginPath()
-    context.moveTo(Math.random() * width, Math.random() * height)
-    context.lineTo(Math.random() * width, Math.random() * height)
+    context.moveTo(w, 0)
+    context.lineTo(w, height)
     context.stroke()
 }
 
-//Make horizontal lines (10px distance between each other) by copying lines 8 to 13 and modifiying the code
-
 //Make vertical lines (10px distance between each other) by copying lines 8 to 13 and modifiying the code
+for (let h = 0; h < height; h+=10) {
+    context.beginPath()
+    context.moveTo(0, h)
+    context.lineTo(width, h)
+    // context.lineTo(h, height)
+    // context.lineTo(width, height)
+    context.stroke()
+}
 
 //Do you have a grid? Remember to comment lines 8 to 13
-
