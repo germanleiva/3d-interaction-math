1cbe8f07f6c275b10160ee62f02fdc874e1664a5
Germán Leiva <leiva@cavi.au.dk>
2021-02-16 14:58:24 +0100

    Orbiting a circle

--- math.js
+++ math.js
@@ -5,28 +5,23 @@ canvas.width = width
 let height = window.innerHeight
 canvas.height = height
 
-let centerY = height / 2
 let centerX = width / 2
-//how far to move the object in each direction
-let offset = 0.5
-//how fast to move the object, the angle incremenet
-let speed = 0.1
+let centerY = height / 2
+let radius = 200
 let angle = 0
-let baseAlpha = 0.5
+let speed = 0.1
+let x
+let y
 
 render()
 
 function render() {
-    //sin gives me values between -1 to 1
-    //when we multiple by offset we get values between -offset to offset
-    //we add to the baseAlpha to start there
-    let alpha = baseAlpha + Math.sin(angle) * offset
-
-    context.fillStyle = `rgba(0,0,0,${alpha})`
     context.clearRect(0,0,width, height)
+    x = centerX + radius * Math.cos(angle)
+    y = centerY + radius * Math.sin(angle)
+
     context.beginPath()
-    // context.arc(x, y, radius, startAngle, endAngle, anticlockwise?)
-    context.arc(centerX, centerY, 100, 0, Math.PI * 2, false)
+    context.arc(x, y, 10, 0, Math.PI * 2, false)
     context.fill()
 
     angle += speed
