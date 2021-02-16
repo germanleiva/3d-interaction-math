56990158db8cfec206a9fe9cb84045cb0237e033
Germán Leiva <leiva@cavi.au.dk>
2021-02-16 13:56:41 +0100

    Added sine wave drawing

--- math.js
+++ math.js
@@ -5,29 +5,12 @@ canvas.width = width
 let height = window.innerHeight
 canvas.height = height
 
-// for (let index = 0; index < 100; index++) {
-//     context.beginPath()
-//     context.moveTo(Math.random() * width, Math.random() * height)
-//     context.lineTo(Math.random() * width, Math.random() * height)
-//     context.stroke()
-// }
+context.translate(0, height / 2)
+context.scale(1,-1) 
 
-//Make horizontal lines (10px distance between each other) by copying lines 8 to 13 and modifiying the code
-for (let w = 0; w < width; w+=10) {
-    context.beginPath()
-    context.moveTo(w, 0)
-    context.lineTo(w, height)
-    context.stroke()
+for (let angle = 0; angle < Math.PI * 2; angle+=.01) {
+    let x = angle * 200
+    let y = Math.sin(angle) * 200
+    console.log(`Point (x=${x},y=${y})`)
+    context.fillRect(x,y,1,1) 
 }
-
-//Make vertical lines (10px distance between each other) by copying lines 8 to 13 and modifiying the code
-for (let h = 0; h < height; h+=10) {
-    context.beginPath()
-    context.moveTo(0, h)
-    context.lineTo(width, h)
-    // context.lineTo(h, height)
-    // context.lineTo(width, height)
-    context.stroke()
-}
-
-//Do you have a grid? Remember to comment lines 8 to 13
