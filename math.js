717e95ef49ed482cd522e1aab707470b37b8247f
Germán Leiva <leiva@cavi.au.dk>
2021-02-16 15:55:10 +0100

    Added an arrow that points to the mouse cursor

--- math.js
+++ math.js
@@ -5,21 +5,41 @@ canvas.width = width
 let height = window.innerHeight
 canvas.height = height
 
-let centerX = width / 2
-let centerY = height / 2
-let radius = 200
+let arrowX = width / 2
+let arrowY = height / 2
+let dx, dy
 let angle = 0
-let x
-let y
-let objectCount = 20
-let slice = Math.PI * 2 / objectCount
 
-for (let i = 0; i < objectCount; i++) {
-    angle = i * slice
-    x = centerX + radius * Math.cos(angle)
-    y = centerY + radius * Math.sin(angle)
+render()
 
+function render() {
+    context.clearRect(0,0,width,height)
+
+    context.save() //save the untransformed context
+    context.translate(arrowX,arrowY) //Translate (aka move) “the whole canvas” so the arrow is at the center 
+    context.rotate(angle) //Rotate “the whole canvas” every time we want to rotate the arrow
+
+    //Draw an arrow
     context.beginPath()
-    context.arc(x, y, 10, 0, Math.PI * 2, false)
-    context.fill()
+    context.moveTo(20, 0)
+    context.lineTo(-20, 0)
+    context.moveTo(20, 0)
+    context.lineTo(10, -10)
+    context.moveTo(20, 0)
+    context.lineTo(10, 10)
+    context.stroke()
+    
+    context.restore() //restore the saved untransformed context to apply the next translate & rotate from scratch
+
+    requestAnimationFrame(render)
 }
+
+document.body.addEventListener("mousemove", function(mouseEvent) {
+    let mouseX = mouseEvent.x
+    let mouseY = mouseEvent.y
+
+    dx = mouseX - arrowX
+    dy = mouseY - arrowY
+
+    angle = Math.atan2(dy,dx)
+})
