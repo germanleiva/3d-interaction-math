5dbefcbe2a847a3f5fd19b67cc1c9d60148c9776
Germán Leiva <leiva@cavi.au.dk>
2021-02-16 14:22:32 +0100

    Animation the circle radius with sine

--- math.js
+++ math.js
@@ -8,22 +8,23 @@ canvas.height = height
 let centerY = height / 2
 let centerX = width / 2
 //how far to move the object in each direction
-let offset = height * .4
+let offset = 50
 //how fast to move the object, the angle incremenet
 let speed = 0.1
 let angle = 0
+let baseRadius = 100
 
 render()
 
 function render() {
     //sin gives me values between -1 to 1
     //when we multiple by offset we get values between -offset to offset
-    //we add to the center to start there
-    let y = centerY + Math.sin(angle) * offset
+    //we add to the baseRadius to start there
+    let radius = baseRadius + Math.sin(angle) * offset
     context.clearRect(0,0,width, height)
     context.beginPath()
     // context.arc(x, y, radius, startAngle, endAngle, anticlockwise?)
-    context.arc(centerX, y, 50, 0, Math.PI * 2, false)
+    context.arc(centerX, centerY, radius, 0, Math.PI * 2, false)
     context.fill()
 
     angle += speed
