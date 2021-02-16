73bb0d963a25dbf30c4248e97cf0d3676df0e461
Germán Leiva <leiva@cavi.au.dk>
2021-02-16 13:15:59 +0100

    Changed width and height of the canvas to the window

---
+++ math.js
@@ -0,0 +1,6 @@
+let canvas = document.getElementById("myCanvas")
+let context = canvas.getContext("2d")
+let width = window.innerWidth
+canvas.width = width
+let height = window.innerHeight
+canvas.height = height
