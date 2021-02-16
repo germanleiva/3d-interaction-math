e49ce2d088b53245162053d2c0c366e4ba11cc8c
Germán Leiva <leiva@cavi.au.dk>
2021-02-16 13:31:25 +0100

    Added code to draw random lines

--- math.js
+++ math.js
@@ -4,3 +4,17 @@ let width = window.innerWidth
 canvas.width = width
 let height = window.innerHeight
 canvas.height = height
+
+for (let index = 0; index < 100; index++) {
+    context.beginPath()
+    context.moveTo(Math.random() * width, Math.random() * height)
+    context.lineTo(Math.random() * width, Math.random() * height)
+    context.stroke()
+}
+
+//Make horizontal lines (10px distance between each other) by copying lines 8 to 13 and modifiying the code
+
+//Make vertical lines (10px distance between each other) by copying lines 8 to 13 and modifiying the code
+
+//Do you have a grid? Remember to comment lines 8 to 13
+
