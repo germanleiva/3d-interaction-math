a75e764db0fdd425e206024e07a80103cf12d05a
Germán Leiva <leiva@cavi.au.dk>
2021-02-26 10:29:23 +0100

    Adding gravity to all the particlesAdding gravity to all the particles

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ let canvas = document.getElementById("myCanvas"),
     width = canvas.width = window.innerWidth,
     height = canvas.height = window.innerHeight,
     particles = [],
-    particleCount = 1,
-    acceleration = new Vector(0.1,0.1)
+    particleCount = 100,
+    gravity = new Vector(0,0.1)
 
 for (let index = 0; index < particleCount; index++) {
-    particles.push(new Particle(100, height, 10, -Math.PI / 2))
+    particles.push(new Particle(width / 2, height / 3, Math.random() * 5 + 2, Math.random() * Math.PI * 2))
 }
 
 update()
@@ -16,10 +16,10 @@ function update() {
     context.clearRect(0, 0, width, height)
 
     for (const p of particles) {
-        p.accelerate(acceleration)
+        p.accelerate(gravity)
         p.update()
         context.beginPath()
-        context.arc(p.position.x, p.position.y, 10, 0, Math.PI * 2)
+        context.arc(p.position.x, p.position.y, 4, 0, Math.PI * 2)
         context.fill()
     }
 
